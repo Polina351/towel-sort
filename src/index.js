3f28fc3a7dc1850877562bea3b417656c4bc052e
Polina Deniskina <polinaoioi67@gmail.com>
2023-08-20 10:38:53 +0300

    feat: create task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix || matrix.length === 0) {
+    return [];
+  }
+  let res = [];
+  for (let i = 0; i < matrix.length; i++) {
+    if (!((i + 1) % 2)) {
+      matrix[i] = matrix[i].reverse();
+    }
+    res = res.concat(matrix[i]);
+  }
+  return res;
 }
